3934f3d879290ae62357bd482738cd8f9e2b6da5
费尔 <admin@ofan.cn>
2019-06-05 13:35:50 +0800

    Dev

--- test.php
+++ test.php
@@ -1,10 +1,9 @@
 <?php
 require './vendor/autoload.php';
-
 use Invoice\BlockChainData;
 
 //创建账本
-$BlockChain = new BlockChainData('BlockChain.dat','KZanthicAtlxIMtgLdTWC07d02Nh3anK');
+$BlockChain = new BlockChainData('BlockChain.dat');
 
 //新增块
 for ($i = 0; $i < 10; $i++) {
@@ -20,6 +19,15 @@ var_dump($BlockChain->count());
 
 //校对账本是否被修改
 var_dump($BlockChain->verifyHash($hash));
+
+/**
+* 分页查询账本
+* @param int $limit 条数
+* @param int $offset 偏移
+* @throws Exception 区块链被破坏
+* @return null|array
+*/
+print_r($BlockChain->find($limit = 1, $offset = 0));
 
 /**
 * 返回所有账本数据
